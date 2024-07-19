e0616a1ad2b1995c23cd7fd8dbb99c1c500f49aa
NagamaniPalemkota <nagamani.palemkota@gmail.com>
2024-07-19 15:06:32 +0530

    committing sgs

---
+++ locals.tf
@@ -0,0 +1,3 @@
+locals {
+  expense_sg_name = "${var.project_name}-${var.environment}-${var.sg_name}"
+}
---
+++ variables.tf
@@ -0,0 +1,43 @@
+variable "project_name" {
+    type = string
+}
+
+variable "environment" {
+    type = string
+ 
+}
+
+variable "sg_name" {
+    type = string
+}
+
+variable "description" {
+    type = string
+}
+
+variable "common_tags" {
+    type = map 
+}
+
+variable "sg_tags" {
+    type = map 
+    default = {}
+}
+variable "vpc_id" {
+    type = string
+}
+variable "outbound_rules" {
+    type = list 
+    default = [
+        {
+        from_port = 0
+        to_port = 0
+        protocol = -1
+        cidr_blocks = ["0.0.0.0/0"]
+    }
+    ]
+}
+variable "inbound_rules" {
+    type = list 
+    default = []
+}
